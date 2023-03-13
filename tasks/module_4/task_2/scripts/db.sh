d98807dda33854d9170d16e71ecd0f94b8874226
valentyn-kinakh <valentyn_kinakh@epam.com>
2023-03-13 20:34:25 +0200

    add backup possibility

--- tasks/module_4/task_2/scripts/db.sh
+++ tasks/module_4/task_2/scripts/db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-filename='users.db'
+filename='../data/users.db'
 
 if [ $1 = "add" ]; then
   echo "Enter user name:"
@@ -13,7 +13,10 @@ if [ $1 = "add" ]; then
   exit
   done
 elif [ $1 = 'backup' ]; then
-  echo $1
+  today=$(date +"%Y-%m-%d")
+  cp $filename "../data/$today-users.db.backup"
+elif [ $1 = 'restore' ]; then
+  find "../data/" -type f -name '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]*' -print0 | xargs -0 ls -tl | head -1 | awk '{print $NF}' | xargs cat > $filename
 elif [[ $1 = "" || $1 = "help" ]]; then
   echo "--add adds user to user.db file"
   echo "--backup creates back up file"

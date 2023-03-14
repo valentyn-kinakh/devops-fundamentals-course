d2514166da5a27035fe06c67df3cb4b329828ea8
valentyn-kinakh <valentyn_kinakh@epam.com>
2023-03-14 14:19:18 +0200

    implement list method

--- tasks/module_4/task_2/scripts/db.sh
+++ tasks/module_4/task_2/scripts/db.sh
@@ -6,26 +6,28 @@ if [ $1 = "add" ]; then
   echo "Enter user name:"
   read name
   echo "Enter user role:"
-  select role in manager developer QA
-  do
-  echo "You have chosen $role"
-  echo "$name ,$role" >> $filename;
-  exit
+  select role in manager developer QA; do
+    echo "You have chosen $role"
+    echo "$name ,$role" >>$filename
+    exit
   done
 elif [ $1 = 'backup' ]; then
   today=$(date +"%Y-%m-%d")
   cp $filename "../data/$today-users.db.backup"
 elif [ $1 = 'restore' ]; then
-  find "../data/" -type f -name '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]*' -print0 | xargs -0 ls -tl | head -1 | awk '{print $NF}' | xargs cat > $filename
+  find "../data/" -type f -name '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]*' -print0 | xargs -0 ls -tl | head -1 | awk '{print $NF}' | xargs cat >$filename
 elif [ $1 = "find" ]; then
   echo "Enter user name:"
-    read name
-  while read -r line
-  do
-    if [[ "$line" == *"$name"*  ]]; then
-        echo "$line"
+  read name
+  while read -r line; do
+    if [[ "$line" == *"$name"* ]]; then
+      echo "$line"
     fi
-  done < "$filename"
+  done <"$filename"
+elif [ $1 = "list" ]; then
+  while read -r line; do
+    echo "$line"
+  done <"$filename"
 elif [[ $1 = "" || $1 = "help" ]]; then
   echo "--add adds user to user.db file"
   echo "--backup creates back up file"
